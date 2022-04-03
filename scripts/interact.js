e2ac5395f3c6d6fd9abf9965cf6e87c9c1ef012c
S2 <shashankshowstoper@gmail.com>
2022-04-04 03:52:12 +0530

    Update interact.js

--- scripts/interact.js
+++ scripts/interact.js
@@ -2,10 +2,20 @@ const {ethers} = require("hardhat");
 
 const API_Key = process.env.API_Key;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// deployed contract address
 const CONTRACT_ADDR = process.env.CONTRACT_ADDR;
+
+// gets contract artifact (contract abi)
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+
+// provider - Alchemy
 const alchemyProvider = new ethers.providers.AlchemyProvider(network="rinkeby", API_Key);
+
+// signer - we
 const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// contract instance
 const contractInstance = new ethers.Contract(CONTRACT_ADDR, contract.abi, signer);
 
 async function main(){
